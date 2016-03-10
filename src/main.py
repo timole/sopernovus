52fe3baece5235aa1ff0d24def8f8ce6657c4ce1
timole <timole@LONDON.solita.fi>
2016-03-10 19:29:16 +0200

    prediction summary

--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='SOPERNOVUS analysator')
     parser.add_argument('-i', '--input-file', help='Input CSV file', required=True)
     parser.add_argument('-o', '--output-file', help='Output CSV file', required=False, default = None)
+    parser.add_argument('-p', '--prediction-output-file', help='Output CSV file for prediction', required=False, default = None)
     args = vars(parser.parse_args())
     return args
 
@@ -25,10 +26,42 @@ def print_stats(startTime):
     elapsedTime = endTime - startTime
     print("Analysis took {} minutes:".format(round(elapsedTime.total_seconds() / 60, 1)))
 
+def create_application_summary(outputFileName):
+    logger = logging.getLogger(__name__)
+
+    # exported to global scope for debugging purposes
+    global appsSummary
+    appsSummary =  analysis.summarize_applications(df)
+
+    logger.info("N of applications = {}".format(len(appsSummary)))
+    print(appsSummary)
+
+    if outputFileName is not None:
+        appsSummary.to_csv(outputFileName, sep=';', encoding='utf-8')
+    else:
+        print appsSummary
+
+def create_prediction_summary(outputFileName):
+    logger = logging.getLogger(__name__)
+
+    # exported to global scope for debugging purposes
+    global predictionSummary
+    predictionSummary =  analysis.summarize_applications(df)
+
+    logger.info("N of applications = {}".format(len(predictionSummary)))
+    print(predictionSummary)
+
+    if outputFileName is not None:
+        predictionSummary.to_csv(outputFileName, sep=';', encoding='utf-8')
+    else:
+        print predictionSummary
+
+
 def main():
     args = parse_args()
     inputFileName = args['input_file']
     outputFileName = args['output_file']
+    predictionOutputFileName = args['prediction_output_file']
 
     utils.log_config()
     logger = logging.getLogger(__name__)
@@ -43,17 +76,8 @@ def main():
 
     logger.info("N of events: {}, from {} to {} ".format(len(df), df['datetime'].min(), df['datetime'].max()))
 
-    # exported to global scope for debugging purposes
-    global appsSummary
-    appsSummary =  analysis.summarize_applications(df)
-
-    logger.info("N of applications = {}".format(len(appsSummary)))
-    print(appsSummary)
-
-    if outputFileName is not None:
-        appsSummary.to_csv(outputFileName, sep=';', encoding='utf-8')
-    else:
-        print appsSummary
+    create_application_summary(outputFileName)
+    create_prediction_summary(predictionOutputFileName)
 
     print_stats(startTime)
 
