0aee4a03be0c015b1e0f600861c3a535e0398275
Timo Lehtonen <timo.lehtonen@solita.fi>
2016-09-12 06:45:36 +0300

    Filter out applications without municipality

--- src/data_helper.py
+++ src/data_helper.py
@@ -7,6 +7,7 @@ logger = logging.getLogger(__name__)
 def import_data(file_name):
     logger.info("Reading data from file " + file_name)
     df = pd.read_csv(file_name, parse_dates=['datetime'], sep = ';')
+    df = df[df["municipalityId"].notnull()]
     df["municipalityId"] = df["municipalityId"].astype(int)
     df["userId"] = df["userId"].astype(int)
     logger.info("N of rows: {:.0f}".format(len(df)))
